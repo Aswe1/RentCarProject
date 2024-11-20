358ecec50901210e4ee76fad092349606fb07b6e
Bo <58004232+Aswe1@users.noreply.github.com>
2024-11-20 11:08:55 +0200

    Base Setup

---
+++ src/main/java/org/example/rentcarproject/RentCarProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.rentcarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarProjectApplication.class, args);
+	}
+
+}
